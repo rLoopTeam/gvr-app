3740f95025be5610acb5d2ee58b4c0f278eb51f9
byte56 <michael@byte56.com>
2017-01-25 19:58:10 -0800

    Adding part label interface. May need to adjust colliders for optimal selection experience.

---
+++ src/Assets/PartLabel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PartLabel : MonoBehaviour, IPointerClickHandler
+{
+
+    public string partText = "";
+    public Text partLabelTextObject;
+
+    void Start()
+    {
+        if (string.IsNullOrEmpty(partText))
+            partText = this.gameObject.name;
+        if (partLabelTextObject == null)
+        { 
+            Debug.Log("You must set a target Text object for text to be displayed!");
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ShowText();
+    }
+
+    public void ShowText()
+    {
+        partLabelTextObject.text = partText;
+    }
+}
